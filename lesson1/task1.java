0a59ce37cea91537fb97db8bce884036674f3717
Дмитрий Костин <z-oo-m@yandex.ru>
2023-04-24 13:05:44 +0200

    Домашняя работа № 1

---
+++ lesson1/task1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class task1{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer n = scanner.nextInt(); 
+        System.out.println(n);
+        scanner.close();
+    }
+}
