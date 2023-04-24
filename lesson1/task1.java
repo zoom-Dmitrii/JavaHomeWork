1dee815bc2e74e0db75ce7928f0c86007eb2a7e9
Дмитрий Костин <z-oo-m@yandex.ru>
2023-04-24 14:03:44 +0200

    Задача 1

--- lesson1/task1.java
+++ lesson1/task1.java
@@ -1,11 +1,29 @@
+// Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
 import java.util.Scanner;
 
 public class task1{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Integer n = scanner.nextInt(); 
-        System.out.println(n);
+        Integer n;
+        System.out.print("Введите значение n: ");
+        n = scanner.nextInt();
+        System.out.printf("Сумма чисел от 1 до %d: %d", n, tringlSum(n));
+        System.out.println("");
+        System.out.printf("Факториал %d!: %d", n, factorial(n));
         scanner.close();
+    }
+
+    public static Integer tringlSum(int n) {
+        if (n == 1) 
+            return n;
+        return n + tringlSum(n-1); 
+    }
+
+    public static Integer factorial(int n) {
+        if (n == 1) 
+            return n;
+        return n * factorial(n-1); 
     }
 }
