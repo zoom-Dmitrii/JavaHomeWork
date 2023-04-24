cf41477b1bb97c646f6aaf9a384b44414fbadeac
Дмитрий Костин <z-oo-m@yandex.ru>
2023-04-24 23:01:11 +0200

    Задача 3

---
+++ lesson1/task3.java
@@ -0,0 +1,39 @@
+//Реализовать простой калькулятор
+import java.util.Scanner;
+
+public class task3 {
+
+    public static String calculation(int num1, int num2, String operator) {
+
+        switch (operator) {
+            case "*":
+                return Integer.toString(num1 * num2);
+            case "/": {
+                if (num2 == 0)
+                    return "Делить на 0 нельзя";
+                else
+                    return Float.toString(num1 / num2);
+            }
+            case "+":
+                return Integer.toString(num1 + num2);
+            case "-":
+                return Integer.toString(num1 - num2);
+        }
+        return "Что-то пошло не так";
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Калькулятор. Введите последовательно: первое число -> оператор -> второе число");
+        System.out.print("Введите первое число: ");
+        int num1 = Integer.parseInt(scanner.nextLine());
+        System.out.print("Введите оператор: ");
+        String operator = scanner.nextLine();
+        System.out.print("Введите второе число: ");
+        int num2 = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+        System.out.println(calculation(num1, num2, operator));
+        scanner.close();
+    }
+}
